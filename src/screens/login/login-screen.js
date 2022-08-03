e88a36a7843511c1f267f003f0e75c40ba40bcba
Angel Hernandez <angel_her_li@hotmail.com>
2022-08-02 22:27:06 -0500

    CHANGE: login view

---
+++ src/screens/login/login-screen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, SafeAreaView, ScrollView} from 'react-native';
+
+const LoginScreen = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <Text>Hola</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default LoginScreen;
